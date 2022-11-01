ae5080a5d39489458d039f354a71153c57386762
ESpriya18 <102942320+ESpriya18@users.noreply.github.com>
2022-11-01 19:04:06 +0530

    Add files via upload

---
+++ FlightTicketBooking/view/ViewFlightDetails.java
@@ -0,0 +1,31 @@
+package view;
+
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import model.*;
+
+public class ViewFlightDetails {
+	protected void viewFlightDetails() 
+	{
+		Statement statement;
+		try {
+			statement = JdbcConnectionClass.getInstance().createStatement();
+			ResultSet flightInfo =  statement.executeQuery("select *from flightdetails");
+
+			System.out.println("FligtId\tFlightName\tBookedTickets\tAvailableTicket\t\tFrom - To\t\tDate and Time\t\tTicketPrice");
+			System.out.println("--------------------------------------------------------------------------------------------------------------------------");
+			
+			while(flightInfo.next())
+			{
+				System.out.println(flightInfo.getInt(1)+"\t"+flightInfo.getString(2)+"\t\t"+flightInfo.getInt(3)+"\t\t"+(50 -flightInfo.getInt(3))+"\t\t"
+						+flightInfo.getString(4)+"\t"+flightInfo.getString(5)+"\t"+flightInfo.getInt(6));
+			}	
+			System.out.println("--------------------------------------------------------------------------------------------------------------------------");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
