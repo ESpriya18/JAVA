9878a2cc4d3a394967bff3726b81dcc554bb4494
ESpriya18 <102942320+ESpriya18@users.noreply.github.com>
2022-11-01 19:02:55 +0530

    Add files via upload

---
+++ FlightTicketBooking/model/JdbcConnectionClass.java
@@ -0,0 +1,25 @@
+package model;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JdbcConnectionClass {
+	
+private static Connection jdbcConnection = null;
+	
+	public static Connection getInstance() 
+	{
+		if(jdbcConnection==null)
+		{
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+			 jdbcConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/flightticketbooking?useSSL=false","root","0001");
+			} catch (SQLException | ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.println(e.getMessage());
+			}
+		}
+		return jdbcConnection;
+	}
+}
